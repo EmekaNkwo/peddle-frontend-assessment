bc9722de539e0fc4ac2c6c6e272680763f842bec
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2022-07-18 03:02:57 +0100

    updated the files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import {render} from "react-dom";
 import "./App.css";
-import AboutPage from "./pages/AboutPage";
-import Blog from "./pages/Blog";
+import AboutPage from "./pages/About/AboutPage";
+import Blog from "./pages/Blog/Blog";
 import ComingSoon from "./pages/ComingSoon/ComingSoon";
-import Contact from "./pages/Contact";
-import Github from "./pages/Github";
+import Contact from "./pages/Contact/Contact";
+import Github from "./pages/GitPage/GitPage";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/coming-soon" element={<ComingSoon />} />
-          <Route path="/" element={<Navigate to="coming-soon" />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/github" element={<Github />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
+          <Route path="coming-soon" element={<ComingSoon />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="github" element={<Github />} />
+          <Route path="contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </>
