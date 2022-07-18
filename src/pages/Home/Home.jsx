cb2afd718b0654841198987271822708aa0f3bcf
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2022-07-18 03:01:30 +0100

    Worked on the home page design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import TASKIMG1 from "../../assets/Screenshot (3).png";
 import { Link } from "react-router-dom";
 import "./Home.css";
 
@@ -13,22 +12,31 @@ function Home() {
             <Link to="/coming-soon">
               <div className="task_item">
                 <h2>Task 1</h2>
-                <div className="task_img">
-                  <img src={TASKIMG1} alt="I-am-here" />
-                </div>
+
                 <div className="task_text">
-                  <p>Lorem ipsum dolor sit amet consectetur ad</p>
+                  <p>
+                    Reproduce the coming soon design prototype in either jsx or
+                    tsx and css <br />
+                    <strong>
+                      "https://xd.adobe.com/view/b9ed8245-2809-4b31-a3c4-5c2e76ddb7e8-ecbe/?fullscreen"
+                    </strong>
+                  </p>
                 </div>
               </div>
             </Link>
             <Link to="/github">
               <div className="task_item">
                 <h2>Task 2</h2>
-                <div className="task_img">
-                  <img src={TASKIMG1} alt="I-am-here" />
-                </div>
                 <div className="task_text">
-                  <p>Lorem ipsum dolor sit amet consectetur ad</p>
+                  <p>
+                    To get the most starred Github repos created in the last 30
+                    days (relative to 2021-09-13), you'll need to call the
+                    following endpoint : <br />
+                    <strong>
+                      https://api.github.com/search/repositories?q=created:&gt;2021-08-13&sort=stars&order=desc
+                      "https://xd.adobe.com/view/b9ed8245-2809-4b31-a3c4-5c2e76ddb7e8-ecbe/?fullscreen"
+                    </strong>
+                  </p>
                 </div>
               </div>
             </Link>
